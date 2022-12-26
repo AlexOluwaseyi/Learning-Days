48db4e7d4b71a8724592197ce16c62db8756066e
Oluwaseyi Akintola <akintolaoluwaseyi34@gmail.com>
2022-12-26 20:27:17 +0100

    Create eloquent.js
    Reading through Eloquent Javascript as I learn programming and software engineering. FIle named after the book.

---
+++ eloquent.js
@@ -0,0 +1,40 @@
+/*
+This is the solution to the fizzbuzz assignment of chapter two of eloquent js. And it worked. D)
+*/
+/*
+let theNumber=1; fizzcount=0; buzzcount=0; fizzbuzzcount=0;
+while (theNumber <=100){
+    if (theNumber%15 == 0){
+        console.log("FizzBuzz");
+        fizzbuzzcount++
+    } 
+    else if (theNumber%3 == 0){
+        console.log("Fizz");
+        fizzcount++
+    }
+    else if (theNumber%5 ==0){
+        console.log("Buzz");
+        buzzcount++
+    }
+    else {
+        console.log(theNumber);
+    }
+    theNumber++;
+    
+}
+console.log("");
+console.log("Total number of FizzBuzz is " + fizzbuzzcount);
+console.log("Total number of Fizz is " + fizzcount);
+console.log("Total number of Buzz is " + buzzcount);
+*/
+
+/*This is my solution to looping a triangle, an assignment in Chapter Two of Eloquent JS.
+And it works. D)
+*/
+/*
+let triangle = ['#', '##', '###', '####', '#####', '######', '#######'], n=0;
+while (n<=7) {
+    console.log(triangle[n]);
+    n++;
+}
+*/
