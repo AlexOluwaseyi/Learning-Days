11b3f3f9e59d23a58b58111b031508b3b481e014
Oluwaseyi Akintola <akintolaoluwaseyi34@gmail.com>
2023-01-02 18:57:53 +0100

    Chapter Four, Exercise One
    This is my codes for the exercises on Chapter Four - Functions of Eloquent Javascript.
    I am currently reading through Eloquent Javascript as I learn Javascript with hopes of becoming a software engineer.

---
+++ Chapter Four (eloquentjs).js
@@ -0,0 +1,18 @@
+/*
+This is my codes for the exercises on Chapter Four - Functions of Eloquent Javascript.
+I am currently reading through Eloquent Javascript as I learn Javascript with hopes of becoming a software engineer.
+
+*/
+
+function min(a,b){
+    if (a < b) {
+        console.log(a);
+    }
+    else if (a > b) {
+        console.log(b);
+    }
+    else {
+        console.log("The two  variables are equal or not comparable.")
+    }
+}
+min(userinput(), userinput())
