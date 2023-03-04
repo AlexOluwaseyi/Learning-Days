b3209120147f6c34f72e434ce156773571671d51
Oluwaseyi Akintola <120264911+AlexOluwaseyi@users.noreply.github.com>
2023-03-04 11:58:41 +0100

    Create main.h

---
+++ Phonebook/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <strings.h>
+
+char *string_toupper(char *n);
+
+
+#endif // MAIN_H
